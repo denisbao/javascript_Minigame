b647bb8d1335000a8a3e2d2c9b50ac9ae61188f9
denisbao <denis.bao@cowmed.com.br>
2022-09-13 15:10:41 -0300

    86 - Adding strong attack funcionality

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,4 +1,5 @@
 const ATTACK_VALUE = 10 //maximum value possible
+const STRONG_ATTACK_VALUE = 17 //maximum value possible
 const MONSTER_ATTACK_VALUE = 15 //maximum value possible
 
 let chosenMaxLife = 100
@@ -8,8 +9,24 @@ let currentPlayerHealth = chosenMaxLife
 adjustHealthBars(chosenMaxLife)
 
 function attackHandler() {
+  attackMonster('ATTACK')
+}
+
+function strongAttackHandler() {
+  attackMonster('STRONG_ATTACK')
+}
+
+function attackMonster(attackMode) {
+  let maxDamage
+
+  if (attackMode === 'ATTACK') {
+    maxDamage = ATTACK_VALUE
+  } else {
+    maxDamage = STRONG_ATTACK_VALUE
+  }
+
   //player attack
-  const damage = dealMonsterDamage(ATTACK_VALUE)
+  const damage = dealMonsterDamage(maxDamage)
   currentMonsterHealth -= damage
 
   //moster attack
@@ -27,3 +44,4 @@ function attackHandler() {
 }
 
 attackBtn.addEventListener('click', attackHandler)
+strongAttackBtn.addEventListener('click', strongAttackHandler)
