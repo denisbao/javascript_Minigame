4bf6e1bc30dafb8d442c27041314e33910be6c8b
denisbao <denis.bao@cowmed.com.br>
2022-09-13 10:53:07 -0300

    85 - Checking win conditions

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,4 +1,5 @@
 const ATTACK_VALUE = 10 //maximum value possible
+const MONSTER_ATTACK_VALUE = 15 //maximum value possible
 
 let chosenMaxLife = 100
 let currentMonsterHealth = chosenMaxLife
@@ -7,8 +8,22 @@ let currentPlayerHealth = chosenMaxLife
 adjustHealthBars(chosenMaxLife)
 
 function attackHandler() {
+  //player attack
   const damage = dealMonsterDamage(ATTACK_VALUE)
   currentMonsterHealth -= damage
+
+  //moster attack
+  const playerDamage = dealPlayerDamage(MONSTER_ATTACK_VALUE)
+  currentPlayerHealth -= playerDamage
+
+  //check for game end
+  if (currentMonsterHealth <= 0 && currentPlayerHealth > 0) {
+    alert('The monster is dead! You are victorious!')
+  } else if (currentPlayerHealth <= 0 && currentMonsterHealth > 0) {
+    alert('You are dead. Pathetic...')
+  } else if (currentPlayerHealth <= 0 && currentMonsterHealth <= 0) {
+    alert('The monster is dead... and you to! Your bravery will be remembered.')
+  }
 }
 
 attackBtn.addEventListener('click', attackHandler)
