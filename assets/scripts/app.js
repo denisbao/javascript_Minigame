00ab0a8abfce0f857a1ecd5885687410851ce319
denisbao <denis.bao@cowmed.com.br>
2022-10-03 09:27:41 -0300

    107 - Throwing custom errors

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -11,12 +11,16 @@ const LOG_EVENT_MONSTER_ATTACK = 'MONSTER_ATTACK'
 const LOG_EVENT_PLAYER_HEAL = 'PLAYER_HEAL'
 const LOG_EVENT_GAME_OVER = 'GAME_OVER'
 
-const enteredValue = prompt('Maximum life for you and the monster:', '100')
-
-let chosenMaxLife = parseInt(enteredValue)
-if (isNaN(chosenMaxLife) || chosenMaxLife <= 10) {
-  chosenMaxLife = 100
+function getMaxLifeValues() {
+  const enteredValue = prompt('Maximum life for you and the monster:', '100')
+  const parseValue = parseInt(enteredValue)
+  if (isNaN(parseValue) || parseValue <= 10) {
+    throw { message: 'Invalid user input, not a number!' }
+  }
+  return parseValue
 }
+
+let chosenMaxLife = getMaxLifeValues()
 
 let currentMonsterHealth = chosenMaxLife
 let currentPlayerHealth = chosenMaxLife
@@ -164,7 +168,22 @@ function healPlayerHandler() {
 }
 
 function printLogHandler() {
-  console.log(battleLog)
+  // for (let i = 0; i < battleLog.length; i++) {
+  //   console.log(battleLog[i])
+  // }
+
+  // for (const logEntry of battleLog) {
+  //   console.log(logEntry)
+  // }
+
+  let i = 1
+  for (const logEntry of battleLog) {
+    console.log(`Log entry: ${i} ---------------------------------------------`)
+    for (const key in logEntry) {
+      console.log(`${key}: ${logEntry[key]}`)
+    }
+    i++
+  }
 }
 
 attackBtn.addEventListener('click', attackHandler)
